0e9187e080e7177f6aed1142cde1214c0a8c4e7b
walther salas <wasalas@hotmail.com>
2022-05-25 11:23:06 -0500

    Inicio del proyecto

--- CapaPresentacion/frmLogin.Designer.cs
+++ CapaPresentacion/frmLogin.Designer.cs
@@ -172,7 +172,7 @@
             // 
             this.lblHora.AutoSize = true;
             this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHora.Location = new System.Drawing.Point(239, 83);
+            this.lblHora.Location = new System.Drawing.Point(194, 83);
             this.lblHora.Name = "lblHora";
             this.lblHora.Size = new System.Drawing.Size(113, 20);
             this.lblHora.TabIndex = 3;
